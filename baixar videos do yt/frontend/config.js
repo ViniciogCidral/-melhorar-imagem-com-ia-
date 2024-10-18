27fdfe89bfe30e46a0a53d8d869d0f2690152525
vinicio <viniciogcidral2@gmail.com>
2024-10-17 21:39:47 -0300

    adicionando parametros para vercel

--- baixar videos do yt/frontend/config.js
+++ baixar videos do yt/frontend/config.js
@@ -1,5 +1,6 @@
 const config = {
-    backendUrl: 'http://localhost:3000'
+    backendUrl: 'http://localhost:3000',
+    pixelCutApiUrl: 'https://api.developer.pixelcut.ai/v1'
 };
 
 export default config;
